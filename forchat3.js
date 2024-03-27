8f8d2017adec038ec4b5dc2ac30093e10a2c73d3
Tiagotj7 <tiagowork07@gmail.com>
2024-03-27 10:41:05 -0300

    send...

--- forchat3.js
+++ forchat3.js
@@ -1,1 +1,11 @@
 alert('Olá,Seja bem vindo!!!')
+
+function enviarMensagem(){
+
+    var mensagem = document.getElementById9("text").value;
+
+    console.log("Mensagem enviada: " + mensagem);
+
+    // Aqui você pode adicionar a lógica para enviar a mensagem para o servidor ou fazer o que desejar com ela
+
+}
