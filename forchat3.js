725b099cf726732f67141917bf6f7cab5f49a438
Tiagotj7 <tiagowork07@gmail.com>
2024-03-27 11:12:08 -0300

    servidor.

--- forchat3.js
+++ forchat3.js
@@ -29,3 +29,17 @@ function limparCaixaTexto(){
     document.getElementById("text").value = " ";
 
 }
+
+const express = require("express");
+
+const app = express();
+
+const port = 3000;
+
+app.use(express.static('public'));
+
+app.listen(port, () => {
+
+    console.log('Servidor rodando dem https://localhost:${port}');
+
+});
