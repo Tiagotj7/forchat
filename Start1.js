1ba107a9f2c71b86708c603f20013c02d33ef196
Tiagotj7 <tiagowork07@gmail.com>
2024-04-12 16:10:07 -0300

    startcssjs

---
+++ Start1.js
@@ -0,0 +1,27 @@
+function validateForm(){
+
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+
+    // Verifica se o usuário e senha correspondem aos valores pré-definidos
+    if (username !== "admin" || password !== "cobaia123"){
+
+        alert("Usuário ou senha incorretos");  
+        return false;// Impede o envio do formulário        
+    }
+
+    return true;// Permite o envio do formulário
+    
+}
+
+function TogglePasswordVisibility(){
+
+    var PasswordInput = document.getElementById("password")
+    if(PasswordInput.type === "password"){
+
+        PasswordInput.type = "text";
+    } else{
+
+        PasswordInput.type = "password";
+    }
+}
