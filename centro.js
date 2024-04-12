b2a6922507cd4ad2d92d4429306c179fb7fc9506
Tiagotj7 <tiagowork07@gmail.com>
2024-04-12 16:12:16 -0300

    centrocssjs

---
+++ centro.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("sala1").onclick = function() {
+            window.location.href = "forchat1.html";
+        };
+
+        document.getElementById("sala2").onclick = function() {
+            window.location.href = "forchat2.html";
+        };
+});
