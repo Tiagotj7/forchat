4ee04681b807464f9f6b5b4c926b648242e17967
Tiagotj7 <tiagowork07@gmail.com>
2024-05-27 00:28:01 -0300

    server1

--- public/server.js
+++ public/server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Iniciar o servidor
-const PORT = process.env.PORT || 9000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
 });
