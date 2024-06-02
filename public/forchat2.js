a81aaf9241672b1df99301be701bce12a8a4f367
Tiagotj7 <tiagowork07@gmail.com>
2024-06-02 00:40:56 -0300

    alert 2

--- public/forchat2.js
+++ public/forchat2.js
@@ -1,3 +1,7 @@
+//Mensagem de alerta ao iniciar a pagina.
+
+alert("Aviso aos Usuários do Chat\n\nRespeito é essencial: não toleramos desrespeito, insultos ou comportamento inadequado.\nSiga as regras e leis: violações resultarão em banimento.\nObrigado por colaborar!");
+
 const messagesInput = document.getElementById('messagesInput');
         const SendButton = document.getElementById('SendButton');
         const messagesContainer = document.getElementById('messages');
