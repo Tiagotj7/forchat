82dc7780b83abf3f2908c13881512723da73c3d2
Tiagotj7 <tiagowork07@gmail.com>
2024-09-20 01:37:38 -0300

    edit tst js

--- public/Start1.js
+++ public/Start1.js
@@ -1,31 +1,34 @@
-function validateForm(){
+function validateForm(event) {
+
+    event.preventDefault(); // Prevenir envio do formulário
 
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
 
     // Verifica se o usuário e senha correspondem aos valores pré-definidos
-    if (username !== "admin" || password !== "cobaia123"){
+    if (username === "admin" && password === "cobaia123"){
 
-        alert("Usuário ou senha incorretos");  
-        return false;// Impede o envio do formulário        
+    windows.location.href = "centro.html"; 
+
+            } else {
+                
+                alert("Usuário ou senha incorretos");  // Impede o envio do formulário        
+
     }
-
-    windows.location.href = "https://forchat.vercel.app/centro.html";
-
-    location.reload();
-
-    //return false;  // Impede o envio do formulário, já que estamos redirecionando manualmente
     
 }
+
+// Adiciona a função validateForm ao evento de submit
+document.getElementById('loginForm').addEventListener('submit', validateForm);
+
 
 function TogglePasswordVisibility(){
 
     var PasswordInput = document.getElementById("password")
     if(PasswordInput.type === "password"){
-
-        PasswordInput.type = "text";
+       PasswordInput.type = "text";
     } else{
-
         PasswordInput.type = "password";
     }
 }
+
